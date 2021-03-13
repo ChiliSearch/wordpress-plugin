7a0c4087b2cd144bbc36a686c46f79cbb3a20952
Ali Jafari <ali.jafari.pw@gmail.com>
2021-03-13 17:47:31 +0100

    refactor plan box in analytics tab

--- templates/admin_tab_analytics.php
+++ templates/admin_tab_analytics.php
@@ -1,6 +1,7 @@
 <?php
 $siteInfo              = ChiliSearch::getInstance()->get_website_info();
-$descriptivePlan       = isset( $siteInfo['descriptivePlan'] ) ? esc_html( $siteInfo['descriptivePlan'] ) : __( 'N/A', 'chilisearch' );
+$plan                  = isset( $siteInfo['plan'] ) ? esc_html( $siteInfo['plan'] ) : __( 'basic', 'chilisearch' );
+$planInfo              = isset( $siteInfo['planInfo'] ) ? esc_html( $siteInfo['planInfo'] ) : '';
 $documentsCount        = isset( $siteInfo['documentsCount'] ) ? esc_html( $siteInfo['documentsCount'] ) : __( 'N/A', 'chilisearch' );
 $usedSpace             = isset( $siteInfo['usedSpace'] ) ? esc_html( $siteInfo['usedSpace'] ) : __( 'N/A', 'chilisearch' );
 $thisMonthRequestCount = isset( $siteInfo['thisMonthRequestCount'] ) ? esc_html( $siteInfo['thisMonthRequestCount'] ) : __( 'N/A', 'chilisearch' );
@@ -8,9 +9,11 @@ $thisMonthRequestCount = isset( $siteInfo['thisMonthRequestCount'] ) ? esc_html(
 <div class="card card-stats">
     <div class="card-header card-header-info card-header-icon">
         <p class="card-category"><?= __( 'Plan', 'chilisearch' ) ?>:</p>
-        <h3 class="card-title"><?= $descriptivePlan ?></h3>
+        <h3 class="card-title"><?= ucfirst($plan) ?><?= !empty($planInfo) ? " <small style='font-weight: normal'>$planInfo</small>" : '' ?></h3>
         <a href="<?= admin_url( 'admin.php?page=chilisearch&tab=plans' ) ?>"><?= __( 'See Plans', 'chilisearch' ) ?> →</a>
-        <a href="https://app.chilisearch.com/websites" target="_blank" style="margin:0 10px;"><?= __( 'Manage', 'chilisearch' ) ?> →</a>
+        <?php if ($plan === 'premium'): ?>
+            <a href="https://app.chilisearch.com/websites" target="_blank" style="margin:0 10px;"><?= __( 'Manage', 'chilisearch' ) ?> →</a>
+        <?php endif; ?>
     </div>
 </div>
 <div class="card card-stats">
