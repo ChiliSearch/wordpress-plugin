7c5c12679966c4bb198c34565805ba180dbf86fa
Ali Jafari <ali.jafari.pw@gmail.com>
2021-04-25 14:48:15 +0200

    - add first draft of Elementor widget

---
+++ widgets/class-widget-elementor-search.php
@@ -0,0 +1,130 @@
+<?php
+namespace ChiliSearch;
+use Elementor\Controls_Manager;
+
+/**
+ * Elementor Chili Search Widget.
+ *
+ * @package ChiliSearch\ElementorSearch
+ * @since 2.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+/**
+ * Elementor Chili Search widget.
+ *
+ * Elementor widget that shows Chili Search form in the page.
+ *
+ * @since 2.0.4
+ */
+class Widget_ElementorSearch extends \Elementor\Widget_Base {
+
+	public function get_name() {
+		return 'chilisearch';
+	}
+
+	public function get_title() {
+		return __( 'Chili Search', 'chilisearch' );
+	}
+
+	public function get_icon() {
+		return 'eicon-site-search';
+	}
+
+	public function get_keywords() {
+		return [ 'search', 'chili', 'chilisearch', 'chili search' , 'ajax' , 'jet' ];
+	}
+
+	public function is_reload_preview_required() {
+		return true;
+	}
+
+	protected function register_controls() {
+		$this->start_controls_section(
+			'section_shortcode',
+			[
+				'label' => __( 'Chili Search', 'chilisearch' ),
+			]
+		);
+
+        $this->add_control(
+            'font',
+            [
+                'label' => __( 'Font', 'chilisearch' ),
+                'type' => Controls_Manager::FONT,
+                'separator' => 'before',
+            ]
+        );
+
+        $this->add_control(
+            'display_submit_button',
+            [
+                'label' => __( 'Display submit button', 'chilisearch' ),
+                'type' => Controls_Manager::SWITCHER,
+                'separator' => 'before',
+                'default' => 'yes',
+            ]
+        );
+
+        $this->add_control(
+            'submit_button_color',
+            [
+                'label' => __( 'Submit button color', 'chilisearch' ),
+                'type' => Controls_Manager::COLOR,
+                'separator' => 'before',
+                'default' => '#ffffff',
+            ]
+        );
+
+        $this->add_control(
+            'submit_button_background_color',
+            [
+                'label' => __( 'Submit button background color', 'chilisearch' ),
+                'type' => Controls_Manager::COLOR,
+                'separator' => 'before',
+                'default' => '#d35050',
+            ]
+        );
+
+        $this->add_control(
+            'submit_button_text',
+            [
+                'label' => __( 'Submit button text', 'chilisearch' ),
+                'type' => Controls_Manager::TEXT,
+                'separator' => 'before',
+                'default' => __( 'Search', 'chilisearch' ),
+            ]
+        );
+
+        $this->add_control(
+            'submit_input_placeholder',
+            [
+                'label' => __( 'Submit input placeholder', 'chilisearch' ),
+                'type' => Controls_Manager::TEXT,
+                'separator' => 'before',
+                'default' => esc_attr_x( 'Search &hellip;', 'placeholder' ),
+            ]
+        );
+
+		$this->end_controls_section();
+	}
+
+	protected function render() {
+        $chilisearch = \ChiliSearch::getInstance();
+        $settings = $this->get_settings_for_display();
+		?>
+        <form action="<?= $chilisearch->get_or_create_search_page() ?>" method="GET" role="search" class="chilisearch-search_form" style="font-family:<?= $settings['font'] ?>">
+            <div class="chlisrch-f-sb">
+                <label class="chilisearch-search_form_label">
+                    <input type="search" placeholder="<?= $settings['submit_input_placeholder'] ?>" name="chilisearch-query" autocomplete="off">
+                </label>
+                <input class="chilisearch-search_form_submit" type="submit" value="<?=$settings['submit_button_text']?>" style="<?= $settings['display_submit_button'] === 'yes'?'':'display:none;' ?>background:<?= $settings['submit_button_background_color'] ?>;color:<?= $settings['submit_button_color'] ?>">
+            </div>
+        </form>
+        <script>try{ChiliSearch.processSearchInputs();}catch(e){}</script>
+        <?php
+	}
+}
