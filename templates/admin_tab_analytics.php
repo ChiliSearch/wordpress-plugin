d0d2fbb0da9ea08ea693d7a9406fa35c0a1789fb
jafarili <ali.jafari.pw@gmail.com>
2021-04-26 20:22:32 +0200

    - refactor analytics tab

--- templates/admin_tab_analytics.php
+++ templates/admin_tab_analytics.php
@@ -6,41 +6,54 @@ $documentsCount        = isset( $siteInfo['documentsCount'] ) ? esc_html( $siteI
 $usedSpace             = isset( $siteInfo['usedSpace'] ) ? esc_html( $siteInfo['usedSpace'] ) : __( 'N/A', 'chilisearch' );
 $thisMonthRequestCount = isset( $siteInfo['thisMonthRequestCount'] ) ? esc_html( $siteInfo['thisMonthRequestCount'] ) : __( 'N/A', 'chilisearch' );
 ?>
-<div class="card card-stats">
-    <div class="card-header card-header-info card-header-icon">
-        <p class="card-category"><?= __( 'Plan', 'chilisearch' ) ?>:</p>
-        <h3 class="card-title"><?= ucfirst( $plan ) ?><?= ! empty( $planInfo ) ? " <small style='font-weight: normal'>$planInfo</small>" : '' ?></h3>
-        <a href="<?= admin_url( 'admin.php?page=chilisearch&tab=plans' ) ?>"><?= __( 'See Plans', 'chilisearch' ) ?> →</a>
-        <a href="javascript:;" onclick="jQuery('#gift-code-holder').slideToggle()" style="margin-left: 10px"><?= __( 'Redeem Gift Code', 'chilisearch' ) ?> →</a>
-        <?php if ( empty( $siteInfo['usedTrialBefore'] ) && $plan === 'basic' ): ?>
-            <a href="javascript:;" onclick="jQuery('#gift-code-holder input').val('trial');jQuery('#gift-code-holder').submit()" style="margin-left: 10px"><?= __( 'Start 7-Days Trial', 'chilisearch' ) ?> →</a>
-        <?php endif; ?>
-        <form style="display: none;margin-top: 10px;" id="gift-code-holder" action="<?= admin_url( 'admin.php?page=chilisearch&tab=analytics' ) ?>" method="post">
-            <input type="text" name="gift-code" placeholder="<?= __( 'Enter Your Gift Code …', 'chilisearch' ) ?>">
-            <button type="submit" class="button button-primary"><?= __( 'Submit', 'chilisearch' ) ?></button>
-        </form>
+<style>
+    .card-holder {
+        margin-top: 30px;
+        margin-bottom: 10px;
+    }
+    .card {
+        margin: 5px;
+        float: left;
+    }
+</style>
+<div class="card-holder">
+    <div class="card card-stats">
+        <div class="card-header card-header-info card-header-icon">
+            <p class="card-category" style="margin: 0;"><?= __( 'Plan', 'chilisearch' ) ?>:</p>
+            <h3 class="card-title" style="margin: .78em 0;"><?= ucfirst( $plan ) ?><?= ! empty( $planInfo ) ? " <small style='font-weight: normal'>$planInfo</small>" : '' ?></h3>
+            <a href="<?= admin_url( 'admin.php?page=chilisearch&tab=plans' ) ?>"><?= __( 'See Plans', 'chilisearch' ) ?> →</a>
+            <a href="javascript:;" onclick="jQuery('#gift-code-holder').slideToggle()" style="margin-left: 10px"><?= __( 'Redeem Gift Code', 'chilisearch' ) ?> →</a>
+            <?php if ( empty( $siteInfo['usedTrialBefore'] ) && $plan === 'basic' ): ?>
+                <a href="javascript:;" onclick="jQuery('#gift-code-holder input').val('trial');jQuery('#gift-code-holder').submit()" style="margin-left: 10px"><?= __( 'Start 7-Days Trial', 'chilisearch' ) ?> →</a>
+            <?php endif; ?>
+            <form style="display: none;margin-top: 10px;" id="gift-code-holder" action="<?= admin_url( 'admin.php?page=chilisearch&tab=analytics' ) ?>" method="post">
+                <input type="text" name="gift-code" placeholder="<?= __( 'Enter Your Gift Code …', 'chilisearch' ) ?>">
+                <button type="submit" class="button button-primary"><?= __( 'Submit', 'chilisearch' ) ?></button>
+            </form>
+        </div>
     </div>
-</div>
-<div class="card card-stats">
-    <div class="card-header card-header-info card-header-icon">
-        <p class="card-category"><?= __( 'Number of Indexed Documents', 'chilisearch' ) ?>:</p>
-        <h3 class="card-title"><?= $documentsCount ?></h3>
+    <div class="card card-stats">
+        <div class="card-header card-header-info card-header-icon">
+            <p class="card-category"><?= __( 'Number of Indexed Documents', 'chilisearch' ) ?>:</p>
+            <h3 class="card-title"><?= $documentsCount ?></h3>
+        </div>
     </div>
-</div>
-<div class="card card-stats">
-    <div class="card-header card-header-success card-header-icon">
-        <p class="card-category">
-            <?= sprintf( __( 'Number of Searches since first of %s', 'chilisearch' ), date( 'F' ) ) ?>
-            <small><?= __( '(updates every 10 minutes)', 'chilisearch' ) ?></small>:
-        </p>
-        <h3 class="card-title"><?= $thisMonthRequestCount ?></h3>
+    <div class="card card-stats">
+        <div class="card-header card-header-success card-header-icon">
+            <p class="card-category">
+                <?= sprintf( __( 'Number of Searches since first of %s', 'chilisearch' ), date( 'F' ) ) ?>
+                <small><?= __( '(updates every 10 minutes)', 'chilisearch' ) ?></small>:
+            </p>
+            <h3 class="card-title"><?= $thisMonthRequestCount ?></h3>
+        </div>
     </div>
-</div>
-<div class="card card-stats">
-    <div class="card-header card-header-warning card-header-icon">
-        <p class="card-category"><?= __( 'Used Space', 'chilisearch' ) ?>
-            <small><?= __( '(updates every 10 minutes)', 'chilisearch' ) ?></small>:</p>
-        <h3 class="card-title"><?= $usedSpace ?></h3>
+    <div class="card card-stats">
+        <div class="card-header card-header-warning card-header-icon">
+            <p class="card-category"><?= __( 'Used Space', 'chilisearch' ) ?>
+                <small><?= __( '(updates every 10 minutes)', 'chilisearch' ) ?></small>:</p>
+            <h3 class="card-title"><?= $usedSpace ?></h3>
+        </div>
     </div>
+    <div style="clear:both;"></div>
 </div>
 <a href="https://wordpress.org/support/plugin/chilisearch/reviews/?filter=5" target="_blank" style="margin-top:20px;display:block;"><?= __( 'Leave us a review', 'chilisearch' ) ?> →</a>
